5c6118d8de5a7f098a1b46c8225f8feea6ea95ea
abhaysinghs772 <abhaysinghs772@gmail.com>
2021-07-17 14:18:49 +0530

    refactor - added my own set of questions and answers

--- fun_quiz.js
+++ fun_quiz.js
@@ -1,45 +1,92 @@
 var readlineSync = require('readline-sync');
 
-// var myName = "Abhay Kumar";
+// var myName = `Abhay Kumar`;
 var userScore = 0;
 
-var userName = readlineSync.question("what is your name : ");
+var userName = readlineSync.question(`what is your name : `);
 
-console.log("welcome " + userName + " !");
+console.log(`hello ${userName}! welcome to my challenge, lets see how well do you know me !
+rules of the game: a question will asked to you and you have to choose the correct option \n`);
 
-// var myAge = 21;
-// var userInput_age = readlineSync.question("what is " + myName + `'s age ? : `);
-
-function checkAnswer(question, myAnswer) {
+function checkAnswer(question, answer, messege) {
   var userAnswer = readlineSync.question(question);
 
-  if (userAnswer == myAnswer) {
+  if (answer == userAnswer){
     userScore++;
-    console.log(`you are right
-      and your score is: ${userScore}`);
-  } else {
+    console.log(`yeah you are correct and your score is ${userScore} 
+    ${messege[0]}`);
+  }else{
     userScore--;
-    console.log(`you are wrong
-      and your score is: ${userScore}`);
+    console.log(`you are not correct and your score is ${userScore} 
+    ${messege[1]}`);
   }
 }
 
-var questions_Answers = 
+var q_and_a_pair =
   [
     {
-      question: "Who is my favorite superhero? ",
-      answer: "Dhruv"
+      question: `what is my age ? 
+      a: 20
+      b: 21
+      c: 19
+      your answer: `,
+      correct_Answer: `b`,
+      messege: [
+        `haa bhaii 21 ka hi hu, kasam se!`,
+        `are bhaii 21 ka hu mai sach me`
+      ]
     },
     {
-      question: "Which is my favorite sad song? ",
-      answer: "Channa Meraya"
+      question: `what i like most? 
+      a: watching web-series
+      b: playing games in mobile
+      c: coding
+      your answer: `,
+      correct_Answer: `c`,
+      messege: [
+        `absolutely right bro`,
+        `eat => code => sleep => repeat`
+      ]
+    },
+    {
+      question: `Am i a marvel fan or a dc fan? 
+      a: marvel
+      b: dc
+      c: both
+      your answer: `,
+      correct_Answer: `a`,
+      messege: [
+        `bhaii hai tu apna`,
+        `teri meri katti, :-(`
+      ]
+    },
+    {
+      question: `ok! but i also like one thing about dc, whats that ? 
+      a: aqua man
+      b: batman
+      c: wonder woman
+      your answer: `,
+      correct_Answer: `c`,
+      messege: [
+        `bade harami ho beta! sahi guess kiye bilkul`,
+        `kab jaa rahe ho swarg ki yatra pe !`
+      ]
+    },
+    {
+      question: `Who is my favorite marvel' superhero? 
+      a: thor
+      b: captain america
+      c: iron-man
+      your answer: `,
+      correct_Answer: `b`,
+      messege: [
+        `captain bro hai apna`,
+        `bilkul galat jawab `
+      ]
     }
   ];
 
-  // console.log(checkAnswer(questions_Answers[0].question, questions_Answers[0].answer));
-
-  for(var i = 0; i < questions_Answers.length; i++ ){
-    var currentQuestion = questions_Answers[i];
-    checkAnswer(currentQuestion.question, currentQuestion.answer);
-  }
-
+for (var i = 0; i < q_and_a_pair.length; i++) {
+  var currentQuestion = q_and_a_pair[i];
+  checkAnswer(currentQuestion.question, currentQuestion.correct_Answer, currentQuestion.messege);
+}
